898802726bd387f9f396bdf950fb346f7b7a2661
subsurface <klemen.remec@gmail.com>
2024-10-10 12:22:45 +0200

    Setup GitHub Actions deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: "/fritata-frontend/",
     plugins: [react()],
     resolve: {
         alias: {
