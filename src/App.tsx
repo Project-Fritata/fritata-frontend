9fc62a9b35176e98cc4a001d5121bf1ea02c6008
subsurface <klemen.remec@gmail.com>
2024-10-10 14:20:53 +0200

    Testing deployment #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center h-screen">
             <h1>FRItata</h1>
             <h2>Social network for FRI students</h2>
         </div>
