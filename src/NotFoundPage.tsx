363c880a95000210b3cafe733051e57ef2b5dd30
subsurface <klemen.remec@gmail.com>
2024-11-21 09:00:13 +0100

    Fixed not-found page

---
+++ src/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import { Heading, VStack } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+
+const NotFoundPage = () => {
+    return (
+        <VStack alignItems={"center"} justifyContent={"center"} height={"100vh"}>
+            <Heading>Not Found: This page does not exist</Heading>
+            <ChakraLink
+                as={ReactRouterLink}
+                to={`/fritata-frontend/`}
+            >
+                Go back to homepage
+            </ChakraLink>
+        </VStack>
+    );
+};
+
+export default NotFoundPage;
